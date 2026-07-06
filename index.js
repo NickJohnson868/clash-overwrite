d31d380715845f701c9c8dfcd0e01d860530e151
johnson <jinsongsun868@gmail.com>
2026-07-06 19:33:21 +0800

    。

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ const customDomainRules = [
   "DOMAIN-SUFFIX,92ydl.com,直连",
   "DOMAIN-SUFFIX,ipzan.com,直连",
   "DOMAIN-SUFFIX,nip.cmliusss,直连",
+  "chat.ant-ling.com,直连",
 
   "DOMAIN-SUFFIX,supercell.com,自选节点",
   "DOMAIN-SUFFIX,grok.com,国外AI",
